c9fc28947c64956cc5822fd4eac2ae162265c72f
Justin Fuller <justindanielfuller@gmail.com>
2014-12-27 02:27:23 +0000

    Home Page Finished

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,19 @@
 class StaticPagesController < ApplicationController
   def home
   end
+  
+  def menu
+  end
+  
+  def reservations
+  end
+  
+  def location
+  end
+  
+  def chef
+  end
+  
+  def contact
+  end
 end
